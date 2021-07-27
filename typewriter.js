10f477dbd0a74c67d910134362aac3aedf315219
Arian <84605909+Ary324@users.noreply.github.com>
2021-07-27 10:16:11 -0400

    Create typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+
+let i = 0;
+
+const typewriter = () => setTimeout(() => {
+  process.stdout.write(sentence[i]);
+  i++;
+  if (i < sentence.length) {
+    typewriter();
+  }
+  if (i === sentence.length) {
+    process.stdout.write('\n');
+  }
+}, 50);
+
+typewriter();
